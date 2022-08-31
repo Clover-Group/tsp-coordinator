8c4ef7f0e5d53891c83c68194644d02f409c933a
trolley813 <trolleybus.1329@gmail.com>
2022-08-31 10:40:12 +0300

    PMM-411 FASTER-based job queue storage

--- Data/JobQueue.cs
+++ Data/JobQueue.cs
@@ -2,10 +2,42 @@ namespace TspCoordinator.Data;
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Text.Json;
+using Dahomey.Json;
+using FASTER.core;
 
 public class JobQueue
 {
     private List<Job> jobs = new List<Job>();
+
+    private readonly FasterKVSettings<string, string> _settings = 
+        new FasterKVSettings<string, string>("/tmp/tsp-coordinator/job-queue") { TryRecoverLatest = true }; 
+    private readonly FasterKV<string, string> _store;
+
+    private string _storeName;
+
+    private readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    }.SetupExtensions();
+
+    public JobQueue()
+    {
+        foreach (var c in TspCoordinator.Data.TspApi.JsonConverters.Converters)
+        {
+            jsonOptions.Converters.Add(c);
+        }
+        _store = new FasterKV<string, string>(_settings);
+        using var s = _store.NewSession(new SimpleFunctions<string, string>((a, b) => b));
+        _storeName = "job_queue";
+        string serialized = "";
+        var st = s.Read(ref _storeName, ref serialized);
+        Console.WriteLine($"Store name: {_storeName}, status: {st}");
+        if (st.Found)
+        {
+            jobs = (List<Job>)JsonSerializer.Deserialize(serialized, typeof(List<Job>));
+        }
+    }
 
     public ReadOnlyCollection<Job> Jobs { get => jobs.AsReadOnly(); }
 
@@ -17,6 +49,8 @@ public class JobQueue
             index++;
         }
         jobs.Insert(index, job);
+        Persist();
+
     } 
     
 
@@ -26,6 +60,7 @@ public class JobQueue
         {
             var res = jobs[0];
             jobs.RemoveAt(0);
+            Persist();
             return res;
         }
         return null;
@@ -41,7 +76,18 @@ public class JobQueue
                 return false;
             case Job job:
                 jobs.Remove(job);
+                Persist();
                 return true;
         }
+    }
+
+    private void Persist()
+    {
+        Console.WriteLine("Persisting...");
+        using var s = _store.NewSession(new SimpleFunctions<string, string>((a, b) => b));
+        var serializedJobs = JsonSerializer.Serialize(jobs);
+        var st = s.Upsert(ref _storeName, ref serializedJobs);
+        _store.Log.Flush(true);
+        Console.WriteLine($"Store name: {_storeName}, status: {st}");
     }
 }
