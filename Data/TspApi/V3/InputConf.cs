6f42848d17c172688560e59b13d0f26e81caf34b
trolley813 <trolleybus.1329@gmail.com>
2022-09-27 10:50:45 +0300

    PMM-520 ass processing batch size

--- Data/TspApi/V3/InputConf.cs
+++ Data/TspApi/V3/InputConf.cs
@@ -12,6 +12,7 @@ public interface IInputConf
     public long? EventsMaxGapMs { get; set; }
     public long? DefaultEventsGapMs { get; set; }
     public long? ChunkSizeMs { get; set; }
+    public int? ProcessingBatchSize { get; set; }
     public ISourceDataTransformation? DataTransformation { get; set; }
     public double? DefaultToleranceFraction { get; set; }
 }
@@ -29,6 +30,7 @@ public class JdbcInputConf : IInputConf
     public long? EventsMaxGapMs { get; set; }
     public long? DefaultEventsGapMs { get; set; }
     public long? ChunkSizeMs { get; set; }
+    public int? ProcessingBatchSize { get; set; }
     public List<string> PartitionFields { get; set; }
     public string? UnitIdField { get; set; }
     public String UserName { get; set; }
@@ -56,6 +58,7 @@ public class KafkaInputConf : IInputConf
     public double? TimestampMultiplier { get; set; }
     public long? EventsMaxGapMs { get; set; }
     public long? ChunkSizeMs { get; set; }
+    public int? ProcessingBatchSize { get; set; }
     public int? NumParallelSources { get; set; }
     public Dictionary<String, String> FieldsTypes { get; set; }
     public long? DefaultEventsGapMs { get; set; }
