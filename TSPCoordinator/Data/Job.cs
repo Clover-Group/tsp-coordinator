975c205d6fb712d51c743041c0d135b78ea97398
trolley813 <trolleybus.1329@gmail.com>
2023-10-30 18:57:10 +0300

    (TSP-332, TSP-334, TSP-336) 1.2.0 release

--- TSPCoordinator/Data/Job.cs
+++ TSPCoordinator/Data/Job.cs
@@ -18,19 +18,35 @@ public class JobStatusInfo
     public long RowsWritten { get; set; }
 }
 
+public class JobMetricPoint
+{
+    public DateTime DateTime { get; set; } = DateTime.Now;
+    public long RowsRead { get; set; }
+    public long RowsWritten { get; set; }
+
+    public static implicit operator JobMetricPoint(JobStatusInfo jobStatusInfo) =>
+        new() { RowsRead = jobStatusInfo.RowsRead, RowsWritten = jobStatusInfo.RowsWritten };
+}
+
 public class Job
 {
     public Job() => Lifecycle = new JobLifecycle(this);
 
-    public string JobId { get; set; } = default! ;
+    public string JobId { get; set; } = default!;
     public JobStatus Status { get; set; }
 
-    public long RowsRead { get; set; } = 0;
-    public long RowsWritten { get; set; } = 0;
+    [JsonIgnore]
+    public List<JobMetricPoint> MetricHistory { get; set; } = new();
+
+    [JsonIgnore]
+    public List<(long, long)> SpeedData { get; set; } = new();
+
+    public long RowsRead => MetricHistory.LastOrDefault()?.RowsRead ?? 0;
+    public long RowsWritten => MetricHistory.LastOrDefault()?.RowsWritten ?? 0;
 
     public bool IsExternal { get; set; } = false;
 
-    public Actual.Request Request { get; set; } = default! ;
+    public Actual.Request Request { get; set; } = default!;
 
     public TspInstance? RunningOn { get; set; }
 
@@ -38,4 +54,26 @@ public class Job
     public JobLifecycle Lifecycle { get; private set; }
 
     public void NotifyStatusChanged() => Lifecycle.AddStatusChanged(Status);
+
+    public void CacheSpeed()
+    {
+        var lastCheckpoints = MetricHistory.TakeLast(10).ToArray();
+        var firstOption = lastCheckpoints.FirstOrDefault();
+        var lastOption = lastCheckpoints.LastOrDefault();
+        var speed = (firstOption, lastOption) switch
+        {
+            (JobMetricPoint first, JobMetricPoint last) =>
+                (
+                    (long)Math.Round((last.RowsRead - first.RowsRead) / (last.DateTime - first.DateTime).TotalSeconds),
+                    (long)Math.Round((last.RowsWritten - first.RowsWritten) / (last.DateTime - first.DateTime).TotalSeconds)
+                ),
+            (_, _) => (0, 0)
+        };
+        if (speed.Item1 < 0) speed.Item1 = 0;
+        if (speed.Item2 < 0) speed.Item2 = 0;
+        SpeedData.Add(speed);
+        if (SpeedData.Count > 10) SpeedData.RemoveAt(0);
+    }
+
+    public (long, long) Speed => SpeedData.LastOrDefault();
 }
--- TSPCoordinator/Data/JobService.cs
+++ TSPCoordinator/Data/JobService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using Dahomey.Json;
+using Prometheus;
 using TspCoordinator.Controllers;
 
 public class PriorityComparer : IComparer<int>
@@ -44,6 +45,20 @@ public class JobService
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
     }.SetupExtensions();
+
+    private readonly Counter rowsReadCounter = 
+        Metrics.CreateCounter("tsp_coordinator_rows_read", "Rows read by a job", "job_id");
+    private readonly Counter rowsWrittenCounter =
+        Metrics.CreateCounter("tsp_coordinator_rows_written", "Rows written by a job", "job_id");
+
+    private readonly Gauge jobReadSpeedGauge =
+        Metrics.CreateGauge("tsp_coordinator_job_read_speed", "Job read speed (rows per second)", "job_id");
+
+    private readonly Gauge jobWriteSpeedGauge =
+        Metrics.CreateGauge("tsp_coordinator_job_write_speed", "Job write speed (rows per second)", "job_id");
+
+    private readonly Gauge jobCountsByStatusGauge =
+        Metrics.CreateGauge("tsp_coordinator_job_count", "Job count (by status)", "status");
 
     public JobService(IHttpClientFactory clientFactory,
                       ILogger<JobService> logger,
@@ -127,8 +142,7 @@ public class JobService
                 job.NotifyStatusChanged();
             }
             job.Lifecycle.AddFinished(info.Success, info.Error);
-            job.RowsRead = info.RowsRead ?? 0;
-            job.RowsWritten = info.RowsWritten ?? 0;
+            UpdateJobMetric(job, new() { RowsRead = info.RowsRead ?? 0, RowsWritten = info.RowsWritten ?? 0 });
             lock (runningJobs) runningJobs.Remove(job);
             lock (completedJobs) completedJobs.Add(job);
             _statusReportingService.SendJobStatus(job, $"Job {job.JobId} completed.");
@@ -190,7 +204,7 @@ public class JobService
     public async void UpdateJobStates(Object? state)
     {
         List<Job> jobsCopy;
-        lock(runningJobs)
+        lock (runningJobs)
         {
             jobsCopy = runningJobs.ToList();
         }
@@ -209,8 +223,8 @@ public class JobService
                     if (response.IsSuccessStatusCode)
                     {
                         var statusInfo = await response.Content.ReadFromJsonAsync<JobStatusInfo>();
-                        job.RowsRead = statusInfo?.RowsRead ?? -1;
-                        job.RowsWritten = statusInfo?.RowsWritten ?? -1;
+                        UpdateJobMetric(job, statusInfo ?? new JobStatusInfo());
+                        
                     }
                     else
                     {
@@ -234,6 +248,11 @@ public class JobService
     {
         try
         {
+            var jobCounts = JobCountsByStatus();
+            foreach(var (status, count) in jobCounts)
+            {
+                jobCountsByStatusGauge.WithLabels(status.ToString().ToUpper()).Set(count);
+            }
             //Console.WriteLine($"Inspecting queue: {jobQueue.Jobs.Count} jobs found");
             while (jobQueue.Jobs.Count > 0)
             {
@@ -279,7 +298,7 @@ public class JobService
                 }
             }
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             _logger.LogWarning($"An exception: `{e.Message}` occurred during queue inspection, skipping that schedule...");
         }
@@ -320,11 +339,42 @@ public class JobService
     {
         var cleanupCompletedInterval = _configurationService.CleanupCompletedInterval;
         var now = DateTime.Now;
-        completedJobs.RemoveAll(job => (now - job.Lifecycle.Events.LastOrDefault().Key).TotalMilliseconds > cleanupCompletedInterval);
+        var jobIdsToRemove = completedJobs
+            .Where(job => (now - job.Lifecycle.Events.LastOrDefault().Key).TotalMilliseconds > cleanupCompletedInterval)
+            .Select(job => job.JobId)
+            .ToArray();
+        foreach (var id in jobIdsToRemove)
+        {
+            rowsReadCounter.RemoveLabelled(id);
+            rowsWrittenCounter.RemoveLabelled(id);
+            jobReadSpeedGauge.RemoveLabelled(id);
+            jobWriteSpeedGauge.RemoveLabelled(id);
+        }
+        completedJobs.RemoveAll(job => jobIdsToRemove.Contains(job.JobId));
     }
 
     public Job? FindJobById(string id) =>
         jobQueue.FindById(id)
             ?? runningJobs.Find(j => j.JobId == id)
             ?? completedJobs.Find(j => j.JobId == id);
+
+    public Dictionary<JobStatus, int> JobCountsByStatus()
+    {
+        return new[] {
+            jobQueue.GetCountsByStatus(),
+            runningJobs.GroupBy(job => job.Status).ToDictionary(group => group.Key, group => group.Count()),
+            completedJobs.GroupBy(job => job.Status).ToDictionary(group => group.Key, group => group.Count())
+        }.SelectMany(c => c).ToLookup(p => p.Key, p => p.Value).ToDictionary(g => g.Key, g => g.Sum());
+    }
+
+    private void UpdateJobMetric(Job job, JobMetricPoint point)
+    {
+        job.MetricHistory.Add(point);
+        rowsReadCounter.WithLabels(job.JobId).IncTo(point.RowsRead);
+        rowsWrittenCounter.WithLabels(job.JobId).IncTo(point.RowsWritten);
+        job.CacheSpeed();
+        var speed = job.Speed;
+        jobReadSpeedGauge.WithLabels(job.JobId).Set(speed.Item1);
+        jobWriteSpeedGauge.WithLabels(job.JobId).Set(speed.Item2);
+    }
 }
