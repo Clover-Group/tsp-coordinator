df5c2971524947c39130713e3b02ae60506b7185
trolley813 <trolleybus.1329@gmail.com>
2025-05-06 17:10:18 +0300

    (HMA0414-16) added null-checking in job queue

--- TSPCoordinator/Data/JobQueue.cs
+++ TSPCoordinator/Data/JobQueue.cs
@@ -57,9 +57,11 @@ public class JobQueue
     public void Enqueue(Job job)
     {
         int index = 0;
-        while (index < jobs.Count && (jobs[index].Request.Priority > job.Request.Priority
+        while (index < jobs.Count && jobs[index]?.Request is not null && (jobs[index].Request.Priority > job.Request.Priority
                || (jobs[index].Request.Priority == job.Request.Priority && jobs[index].ArrivalTime < job.ArrivalTime)))
         {
+            if (jobs[index] is null) Console.WriteLine($"Null occured in the job queue at position {index}");
+            else if (jobs[index].Request is null) Console.WriteLine($"Request for job {job.JobId} is null");
             index++;
         }
         jobs.Insert(index, job);
